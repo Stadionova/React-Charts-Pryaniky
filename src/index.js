435018b2fa30588e859b06d3d45d520f2d433eb5
Stadionova <726383@mail.ru>
2019-10-30 13:08:27 +0300

    pluged google charts to react project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Chart from "react-google-charts";
 
-import { render } from "react-dom";
+const data = [
+    ["Year", "Visitations", { role: "style" }],
+    ["2010", 10, "color: gray"],
+    ["2020", 14, "color: #76A7FA"],
+    ["2030", 16, "color: blue"],
+    ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+    [
+        "2050",
+        28,
+        "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+    ]
+];
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <Chart chartType="BarChart" width="100%" height="400px" data={data} />
+            </div>
+        );
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
