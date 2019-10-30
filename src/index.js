6c100981b28de3400b2d5ad24ea52dedc06a4c15
Stadionova <726383@mail.ru>
2019-10-30 13:51:54 +0300

    added buttons for year/month/day

--- src/index.js
+++ src/index.js
@@ -2,23 +2,67 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Chart from "react-google-charts";
 
-const data = [
-    ["Year", "Visitations", { role: "style" }],
-    ["2010", 10, "color: gray"],
-    ["2020", 14, "color: #76A7FA"],
-    ["2030", 16, "color: blue"],
-    ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
-    [
-        "2050",
-        28,
-        "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
-    ]
-];
+// const data = [
+//     ["Year", "Visitations", { role: "style" }],
+//     ["2010", 10, "color: gray"],
+//     ["2020", 14, "color: #76A7FA"],
+//     ["2030", 16, "color: blue"],
+//     ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+//     [
+//         "2050",
+//         28,
+//         "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"
+//     ]
+// ];
+
 class App extends React.Component {
+
+    constructor() {
+
+        super();
+
+        this.state = {
+            data: [
+                ["Year", "Visitations", { role: "style" }],
+                ["2010", 10, "color: gray"],
+                ["2020", 14, "color: #76A7FA"],
+                ["2030", 16, "color: blue"],
+                ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+                ["2050", 28, "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"]
+            ]
+        };
+
+        this.pushNewIdeaByButton = this.pushNewIdeaByButton.bind(this);
+    }
+
+    pushNewIdeaByButton(event) {
+        this.setState({
+            data: [
+                ["Month", "Visitations", { role: "style" }],
+                ["January", 10, "color: gray"],
+                ["February", 14, "color: #76A7FA"],
+                ["2030", 16, "color: blue"],
+                ["2040", 22, "stroke-color: #703593; stroke-width: 4; fill-color: #C5A5CF"],
+                ["2050", 28, "stroke-color: #871B47; stroke-opacity: 0.6; stroke-width: 8; fill-color: #BC5679; fill-opacity: 0.2"]
+            ]
+        });
+    };
+
     render() {
         return (
-            <div className="App">
-                <Chart chartType="BarChart" width="100%" height="400px" data={data} />
+            <div>
+                <div className="App">
+                    <Chart chartType="BarChart" width="100%" height="400px" data={this.state.data} />
+                </div>
+                <div>
+                    <button onClick={this.pushNewIdeaByButton.bind(this)}>По годам</button>
+                </div>
+                <div>
+                    <button onClick={this.pushNewIdeaByButton.bind(this)}>По месяцам</button>
+                </div>
+                <div>
+                    <button onClick={this.pushNewIdeaByButton.bind(this)}>По дням</button>
+                </div>
             </div>
         );
     }
