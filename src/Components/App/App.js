d8bc79801c1b85b725c0dd20fbdcf00f85251e42
Stadionova <726383@mail.ru>
2019-11-01 15:32:43 +0300

    try to correct block

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Chart from "../Chart/Chart";
 import { data } from '../../modules/data.js';
-// import { getDataYear, getDataMonth, getDataDay } from '../../dataApi';
 import ChartPeriods from '../ChartPeriods/ChartPeriods';
 
 class App extends React.Component {
@@ -39,18 +38,9 @@ class App extends React.Component {
                             <div>
                                 <Chart data={this.state.data} />
                             </div>
-                            <ChartPeriods data={this.state.data} onChangeDataPeriod={this.changeDataPeriod} />
-                            {/* <div className='main__buttons-periods'>
-                                <div>
-                                    <button onClick={this.changeDataChartYear.bind(this)}>По годам</button>
-                                </div>
-                                <div>
-                                    <button onClick={this.changeDataChartMonth.bind(this)}>По месяцам</button>
-                                </div>
-                                <div>
-                                    <button onClick={this.changeDataChartDay.bind(this)}>По дням</button>
-                                </div>
-                            </div> */}
+                            <div>
+                                <ChartPeriods data={this.state.data} onChangeDataPeriod={this.changeDataPeriod} />
+                            </div>
                         </div>
                     </main>
                 </div>
--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleChart from "react-google-charts";
 import ChartTypes from '../ChartTypes/ChartTypes';
+import './Chart.css';
 
 class Chart extends React.Component {
 
@@ -24,20 +25,24 @@ class Chart extends React.Component {
 
     render() {
         return (
-            <div>
-                <GoogleChart options={{
-                    title: 'Population of Largest U.S. Cities',
-                    chartArea: { width: '50%' },
-                    hAxis: {
-                        title: 'Total Population',
-                        minValue: 0,
-                    },
-                    vAxis: {
-                        title: 'City',
-                    },
-                    legend: { position: 'none' },
-                }} chartType={this.state.chartType} width="900px" height="400px" data={this.props.data} />
-                <ChartTypes chartType={this.state.chartType} onChangeChart={this.changeChart} />
+            <div className="container">
+                <div className="container__chart">
+                    <GoogleChart options={{
+                        title: 'Population of Largest U.S. Cities',
+                        chartArea: { width: '50%' },
+                        hAxis: {
+                            title: 'Total Population',
+                            minValue: 0,
+                        },
+                        vAxis: {
+                            title: 'City',
+                        },
+                        legend: { position: 'none' },
+                    }} chartType={this.state.chartType} width="1000px" height="400px" data={this.props.data} />
+                </div>
+                <div className="container__chartTypes">
+                    <ChartTypes chartType={this.state.chartType} onChangeChart={this.changeChart} />
+                </div>
             </div>
         );
     }
